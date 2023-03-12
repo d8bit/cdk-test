919713b1eca6905e019d77ae4067d737085f4645
Deibit <doliva@refineriaweb.com>
2023-03-12 19:18:02 +0100

    Saving state in S3

--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
 import { Construct } from "constructs";
-import { App, TerraformStack, TerraformOutput } from "cdktf";
+import { App, TerraformStack, TerraformOutput, S3Backend } from "cdktf";
 import { AwsProvider } from "@cdktf/provider-aws/lib/provider";
 import { Instance } from "@cdktf/provider-aws/lib/instance";
+// import { S3Bucket } from "@cdktf/provider-aws/lib/s3-bucket";
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
@@ -12,9 +13,13 @@ class MyStack extends TerraformStack {
     });
 
     const ec2Instance = new Instance(this, "compute", {
-      ami: "ami-01456a894f71116f2",
+      ami: "ami-08fea9e08576c443b",
       instanceType: "t2.micro",
     });
+
+    // new S3Bucket(this, "bucket", {
+    //   bucketPrefix: "myCdkStates",
+    // });
 
     new TerraformOutput(this, "public_ip", {
       value: ec2Instance.publicIp,
@@ -24,6 +29,7 @@ class MyStack extends TerraformStack {
 
 const app = new App();
 const stack = new MyStack(app, "aws_instance");
+// new MyStack(app, "aws_instance");
 
 // new RemoteBackend(stack, {
 //   hostname: "app.terraform.io",
@@ -32,6 +38,14 @@ const stack = new MyStack(app, "aws_instance");
 //     name: "learn-cdktf",
 //   },
 // });
+
+
+new S3Backend(stack, {
+    bucket: "mycdkstates",
+    key   : "ec2instances",
+    region: "eu-west-1"
+  }
+);
 
 app.synth();
 
